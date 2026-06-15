e886ea731d172cd99ed1df13ab1c153d38c78694
JOSIAH <josiahjonathan2309@gmail.com>
2026-06-15 12:11:35 +0300

    Update backend main.py file

--- backend/main.py
+++ backend/main.py
@@ -213,3 +213,4 @@ def predict_expense_category(payload: PredictPayload):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    
